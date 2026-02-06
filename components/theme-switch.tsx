735106808c37e19008058c65e09fc230f54f6921
Haeyzed <59699632+Haeyzed@users.noreply.github.com>
2026-02-06 02:46:01 +0100

    Update theme-switch.tsx

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -53,9 +53,9 @@ export function ThemeSwitch() {
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
-        <Button variant='ghost' size='icon' className='scale-95 rounded-full'>
-          <HugeiconsIcon icon={Sun01Icon} className='size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
-          <HugeiconsIcon icon={Moon01Icon} className='absolute size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+        <Button variant='ghost' size='icon' className='relative scale-95 rounded-full'>
+          <HugeiconsIcon icon={Sun01Icon} className='size-[1.2rem] scale-100 rotate-0 transition-all duration-300 dark:scale-0 dark:-rotate-90 dark:opacity-0' />
+          <HugeiconsIcon icon={Moon01Icon} className='absolute inset-0 m-auto size-[1.2rem] scale-0 rotate-90 opacity-0 transition-all duration-300 dark:scale-100 dark:rotate-0 dark:opacity-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>

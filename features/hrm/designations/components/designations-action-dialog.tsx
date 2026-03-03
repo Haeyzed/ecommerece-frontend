45a2ade208e37c9c0cf4554bdabdb93c37ab818d
Haeyzed <muibi.azeezabolade@gmail.com>
2026-03-04 00:27:41 +0100

    update

--- features/hrm/designations/components/designations-action-dialog.tsx
+++ features/hrm/designations/components/designations-action-dialog.tsx
@@ -3,7 +3,14 @@
 import React, { useState } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Controller, useForm, type UseFormReturn } from 'react-hook-form'
-
+import {
+  CancelCircleIcon,
+  CloudUploadIcon,
+  PlusSignIcon,
+  Delete02Icon,
+  File02Icon
+} from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/react'
 import {
   useCreateDesignation,
   useUpdateDesignation
